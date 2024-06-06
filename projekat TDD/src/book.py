3947fdcf5043add73ed7f289265e8e357ea7c3a0
ssskrbic <144312629+ssskrbic@users.noreply.github.com>
2024-06-06 11:19:44 +0200

    Update book.py

--- projekat TDD/src/book.py
+++ projekat TDD/src/book.py
@@ -1,6 +1,1 @@
-class Knjiga:
-    def __init__(self, naslov, autor, god_izdavanja, zanr):
-        self.naslov = naslov
-        self.autor = autor
-        self.god_izdavanja = god_izdavanja
-        self.zanr = zanr
+
